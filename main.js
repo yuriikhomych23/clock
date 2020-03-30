81238d49eabc413746b78828d3f96ab313f0dd11
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-03-30 23:29:25 +0000

    Sample code

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-
-let stopwatch = document.querySelector('.stopwatch');
-let clock = document.querySelector('.clock');
-
-
-
+//CLOCK 
 setInterval(function() {
     let clock = new Date();
     let hours = clock.getHours();
@@ -16,6 +11,8 @@ setInterval(function() {
     times.innerHTML = hours + ':' + minutes + ':' + seconds
 }, 1000)
 
+
+//DATE
 setInterval(function() {
     let date = new Date ();
     let day = date.getDate();
@@ -31,35 +28,141 @@ setInterval(function() {
 }, 1000);
 
 
+//STOPWATCH
 
+let millisecond = 0;
 let sec = 0;
 let min = 0;
 let hr = 0;
+let sw;  // StopWatch
 
-let startClock = function stopWatch() {
+let btnStart = document.getElementById('start')
+let btnLoop = document.getElementById('loop');
+let btnStop = document.getElementById('stop');
+let btnReset = document.getElementById('reset');
+
+let result = document.querySelector('.result')
+let timer = document.querySelector('.timer');
+
+
+//BTN START
+btnStart.addEventListener('click', function() {
+    sw = setInterval(stopwatch, 10);
+})
+
+//BTN STOP
+btnStop.addEventListener('click', function() {
+    clearInterval(sw);
+})
+
+// BTN RESET
+btnReset.addEventListener('click', function () {
+    clearInterval(sw);
+    timer.innerHTML = '00' + ':' + '00' + ':' + '00' + ':' + '000';
+    result.innerHTML = '';
+    millisecond = 0;
+    sec = 0;
+    min = 0;
+    hr = 0;
+})
+
+//BTN LOOP
+btnLoop.addEventListener('click', function() {
+    let div = document.createElement('div');
+    div.innerHTML += timer.innerHTML;
+    result.appendChild(div);
+});
+
+
+//FUNCTION STOPWATCH
+function stopwatch() {
+    millisecond++;
+    if(millisecond == 100) {
+        millisecond = 0;
         sec++;
-        if(sec / 60 === 1) {
-            sec = 0;
-            min++;
-            if(min / 60 === 1) {
-                min = 0;
-                hr++;
-            }
-        }
-
-        document.querySelector('.zero').innerHTML = hr + ':' + min + ':' + sec;
     }
+    if(sec == 60) {
+        min++;
+        sec = 0;
+    }
+    if(min == 60) {
+        hr++;
+        min = 0;
+    }
+    if (hr < 10) hr = "0" + parseFloat(hr);
+    if (min < 10) min = "0" + parseFloat(min);
+    if (sec < 10) sec = "0" + parseFloat(sec);
+    if (millisecond < 10) millisecond = "0" + parseFloat(millisecond);
+    timer.innerHTML = hr + ":" + min + ":" + sec + ":" + millisecond;
+}
 
-start.addEventListener('click', function () {
-    setInterval(startClock, 1000);
+
+
+//TIMER 
+//btn plus minus
+let btnPlus = document.querySelector('.plus');
+let btnMinus = document.querySelector('.minus')
+
+
+
+let counting = document.querySelector('.counting');
+let countingTimer = counting.innerHTML;
+
+//BTN PLUS
+btnPlus.addEventListener('click', function() {
+    counting.innerHTML++;
+})
+
+//BTN MINUS
+btnMinus.addEventListener('click', function(){
+    counting.innerHTML--;
+    if(counting.innerHTML == 0) {
+        alert('Smaller countdown is impossible');
+        counting.innerHTML = 1;
+    }
+})
+
+
+//btn timer 
+let btnStartTimer = document.querySelector('.start__timer');
+let btnStopTimer = document.querySelector('.stop__timer');
+let btnResetTimer = document.querySelector('.reset__timer');
+
+
+let secondsTimer = 60;
+let countdown;
+
+//BTN START TIMER 
+btnStartTimer.addEventListener('click', function() {
+    countdown = setInterval(countdown, 1000)
+})
+
+//BTN STOP TIMER 
+btnStopTimer.addEventListener('click', function() {
+    clearInterval(countdown);
+})
+
+//BTN RESET TIMER 
+btnResetTimer.addEventListener('click', function() {
+    clearInterval(countdown);
+    nuww.innerHTML = "00" + ":" + '00'
 })
 
 
 
-document.getElementById('stop').addEventListener('click', function() {
-    clearInterval(startClock);
-    console.log('Привіт')
-})
+function countdownTimer() {
+
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
